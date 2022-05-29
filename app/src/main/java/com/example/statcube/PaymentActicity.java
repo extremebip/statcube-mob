ef62f1c626212ae0021b553978511893d421256d
Nicholas <nicholasrazalii@gmail.com>
2022-05-29 12:55:14 +0700

    payment

---
+++ app/src/main/java/com/example/statcube/PaymentActicity.java
@@ -0,0 +1,26 @@
+package com.example.statcube;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+public class PaymentActicity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_payment_acticity);
+
+
+        Spinner spinnerPayment =findViewById(R.id.spinner_payments);
+        ArrayAdapter<CharSequence>adapter= ArrayAdapter.createFromResource(this, R.array.payment, android.R.layout.simple_spinner_item);
+
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_item);
+
+
+        spinnerPayment.setAdapter(adapter);
+
+    }
+}
